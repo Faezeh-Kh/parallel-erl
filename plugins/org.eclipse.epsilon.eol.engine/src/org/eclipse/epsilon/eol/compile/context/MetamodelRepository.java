d568cd52005d9cb4d15fed32e3a23754744812a9
Sina Madani <sinadoom@gmail.com>
2018-03-30 13:15:17 +0100

    Removed source files, as they are now available on the main Epsilon repository.

--- plugins/org.eclipse.epsilon.eol.engine/src/org/eclipse/epsilon/eol/compile/context/MetamodelRepository.java
+++
@@ -1,7 +0,0 @@
-package org.eclipse.epsilon.eol.compile.context;
-
-public class MetamodelRepository {
-	
-	// protected HashMap<String, List<Metamodel>> cache = new HashMap<String, List<Metamodel>>();
-	
-}
--- plugins/org.eclipse.epsilon.evl.engine/src/org/eclipse/epsilon/evl/execute/context/concurrent/IEvlContextParallel.java
+++
@@ -1,8 +0,0 @@
-package org.eclipse.epsilon.evl.execute.context.concurrent;
-
-import org.eclipse.epsilon.erl.execute.context.concurrent.IErlContextParallel;
-import org.eclipse.epsilon.evl.execute.context.IEvlContext;
-
-public interface IEvlContextParallel extends IEvlContext, IErlContextParallel {
-
-}
