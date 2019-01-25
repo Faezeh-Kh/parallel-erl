93b7f3a389c408ddb68aee0c5711079bfa4f49e4
SMadani <sinadoom@googlemail.com>
2019-01-25 17:02:31 +0000

    Tidied up ImdbQueryBenchmark

--- plugins/java-benchmarks/src/ImdbQueryBenchmark.java
+++ plugins/java-benchmarks/src/ImdbQueryBenchmark.java
@@ -1,11 +1,13 @@
 import java.util.Collection;
 import java.util.Set;
 import java.util.stream.StreamSupport;
-import org.eclipse.emf.ecore.EObject;
+import org.eclipse.epsilon.common.launch.ProfilableRunConfiguration;
+import org.eclipse.epsilon.common.util.profiling.BenchmarkUtils;
 import org.eclipse.epsilon.emc.emf.EmfModel;
 import org.eclipse.epsilon.eol.exceptions.EolRuntimeException;
 import org.eclipse.epsilon.eol.execute.introspection.IPropertyGetter;
 import org.eclipse.epsilon.eol.execute.operations.contributors.IterableOperationContributor;
+import org.eclipse.epsilon.eol.function.CheckedEolRunnable;
 import org.eclipse.epsilon.eol.types.EolSet;
 
 /*********************************************************************
@@ -17,39 +19,81 @@ import org.eclipse.epsilon.eol.types.EolSet;
  *
  * SPDX-License-Identifier: EPL-2.0
 **********************************************************************/
-public class ImdbQueryBenchmark {
+public class ImdbQueryBenchmark extends ProfilableRunConfiguration {
 
-	static IPropertyGetter propertyGetter;
-	static int threshold = 3;
-	
-	public static void main(String... args) throws Exception {
-		if (args.length < 3) throw new IllegalArgumentException("Must include path to model, metamodel and whether to parallelise!");
-		var modelPath = args[0];
-		var metamodelPath = args[1];
-		var parallel = Boolean.valueOf(args[1]);
+	static class Builder extends ProfilableRunConfiguration.Builder<ImdbQueryBenchmark, Builder> {
+		EmfModel model;
+		boolean parallel;
 		
-		var model = new EmfModel();
-		model.setCachingEnabled(true);
-		model.setConcurrent(true);
-		model.setModelFile(modelPath);
-		model.setMetamodelFile(metamodelPath);
-		model.load();
-		propertyGetter = model.getPropertyGetter();
+		public Builder parallel(boolean p) {
+			this.parallel = p;
+			return this;
+		}
 		
-		var result = StreamSupport.stream(model.getAllOfKind("Person").spliterator(), parallel)
-			.filter(a -> coactors(a).stream().anyMatch(co -> areCoupleCoactors(a, co)))
-			.count();
+		public Builder withModel(String modelPath, String metamodelPath) {
+			model = new EmfModel();
+			model.setCachingEnabled(true);
+			model.setConcurrent(true);
+			model.setModelFile(modelPath);
+			model.setMetamodelFile(metamodelPath);
+			return this;
+		}
 		
-		System.out.println(result);
+		@Override
+		public ImdbQueryBenchmark build() throws IllegalArgumentException, IllegalStateException {
+			return new ImdbQueryBenchmark(this);
+		}
 	}
 	
-	@SuppressWarnings("unchecked")
-	static Set<? extends EObject> coactors(EObject self) {
+	ImdbQueryBenchmark(Builder builder) {
+		super(builder);
+		this.propertyGetter = (this.model = builder.model).getPropertyGetter();
+		this.parallel = builder.parallel;
+	}
+	
+	final EmfModel model;
+	final IPropertyGetter propertyGetter;
+	final boolean parallel;
+	int threshold = 3;
+	
+	@Override
+	protected void preExecute() throws Exception {
+		super.preExecute();
+		BenchmarkUtils.profileExecutionStage(profiledStages, "Model loading", (CheckedEolRunnable) model::load);
+	}
+	
+	@Override
+	protected Void execute() throws Exception {
+		BenchmarkUtils.profileExecutionStage(profiledStages, "execute()", new CheckedEolRunnable() {
+			@Override
+			public void runThrows() throws EolRuntimeException {
+				
+				var result = StreamSupport.stream(model.getAllOfKind("Person").spliterator(), parallel)
+					.filter(a -> coactors(a).stream().anyMatch(co -> areCoupleCoactors(a, co)))
+					.count();
+				System.out.println(result);
+			}
+		});
+		
+		return null;
+	}
+	
+	public static void main(String... args) throws Exception {
+		if (args.length < 4) throw new IllegalArgumentException("Must include path to model, metamodel, whether to parallelise and output file!");
+		var modelPath = args[0];
+		var metamodelPath = args[1];
+		var parallel = Boolean.valueOf(args[2]);
+		var resultsFile = args[3];
+		
+		new Builder().withOutputFile(resultsFile).withProfiling().withModel(modelPath, metamodelPath).parallel(parallel).build().run();
+	}
+	
+	Set<?> coactors(Object self) {
 		try {
-			var movies = (Collection<? extends EObject>) propertyGetter.invoke(self, "movies");
-			var results = new EolSet<EObject>();
+			var movies = (Collection<?>) propertyGetter.invoke(self, "movies");
+			var results = new EolSet<>();
 			for (var movie : movies) {
-				for (var moviePerson : ((Iterable<? extends EObject>) propertyGetter.invoke(movie, "persons"))) {
+				for (var moviePerson : ((Iterable<?>) propertyGetter.invoke(movie, "persons"))) {
 					results.add(moviePerson);
 				}
 			}
@@ -60,14 +104,13 @@ public class ImdbQueryBenchmark {
 		}
 	}
 	
-	@SuppressWarnings("unchecked")
-	static boolean areCoupleCoactors(EObject self, EObject co) {
+	boolean areCoupleCoactors(Object self, Object co) {
 		try {
 			var selfName = (String) propertyGetter.invoke(self, "name");
 			var coName = (String) propertyGetter.invoke(co, "name");
 			var comparison = selfName.compareTo(coName);
 			if (comparison < 0) {
-				var bMovies = (Collection<? extends EObject>) propertyGetter.invoke(co, "movies");
+				var bMovies = (Collection<?>) propertyGetter.invoke(co, "movies");
 				if (bMovies.size() >= threshold) {
 					return areCouple(self, co);
 				}
@@ -79,10 +122,9 @@ public class ImdbQueryBenchmark {
 		}
 	}
 	
-	@SuppressWarnings("unchecked")
-	static boolean areCouple(EObject self, EObject p) throws EolRuntimeException {
-		var selfMovies = (Collection<? extends EObject>) propertyGetter.invoke(self, "movies");
-		var pMovies = (Collection<? extends EObject>) propertyGetter.invoke(p, "movies");
+	boolean areCouple(Object self, Object p) throws EolRuntimeException {
+		var selfMovies = (Collection<?>) propertyGetter.invoke(self, "movies");
+		var pMovies = (Collection<?>) propertyGetter.invoke(p, "movies");
 		var excludingPMoviesSize = new IterableOperationContributor(selfMovies).excludingAll(pMovies).size();
 		var targetSize = selfMovies.size() - threshold;
 		return excludingPMoviesSize <= targetSize;
