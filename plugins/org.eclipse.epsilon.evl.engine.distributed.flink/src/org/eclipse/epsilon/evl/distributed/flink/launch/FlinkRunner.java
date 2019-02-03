1e1f511b9095cf93339bcfac441844e3eff661c3
SMadani <sinadoom@googlemail.com>
2019-02-03 22:10:11 +0000

    Split up Flink and JMS EVL into separate PDE projects.

---
+++ plugins/org.eclipse.epsilon.evl.engine.distributed.flink/src/org/eclipse/epsilon/evl/distributed/flink/launch/FlinkRunner.java
@@ -0,0 +1,55 @@
+/*********************************************************************
+ * Copyright (c) 2019 The University of York.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+**********************************************************************/
+package org.eclipse.epsilon.evl.distributed.flink.launch;
+
+import org.eclipse.epsilon.eol.cli.EolConfigParser;
+import org.eclipse.epsilon.evl.distributed.EvlModuleDistributedMaster;
+import org.eclipse.epsilon.evl.distributed.flink.atomic.EvlModuleDistributedFlinkAtoms;
+import org.eclipse.epsilon.evl.distributed.flink.batch.EvlModuleDistributedFlinkSubset;
+import org.eclipse.epsilon.evl.distributed.launch.DistributedRunner;
+import org.eclipse.epsilon.evl.launch.EvlRunConfiguration;
+
+/**
+ * 
+ * @author Sina Madani
+ * @since 1.6
+ */
+public class FlinkRunner extends DistributedRunner {
+
+	public static void main(String[] args) throws ClassNotFoundException {
+		String modelPath = args[1].contains("://") ? args[1] : "file:///"+args[1];
+		String metamodelPath = args[2].contains("://") ? args[2] : "file:///"+args[2];
+		
+		EolConfigParser.main(new String[] {
+			"CONFIG:"+DistributedRunner.class.getName(),
+			args[0],
+			"-models",
+				"\"emf.DistributableEmfModel#"
+				+ "concurrent=true,cached=true,readOnLoad=true,storeOnDisposal=false,"
+				+ "modelUri="+modelPath+",fileBasedMetamodelUri="+metamodelPath+"\"",
+			args.length > 5 ? "-outfile" : "",
+			args.length > 5 ? args[5] : "",
+			"-module",
+				(args.length > 4 && (
+					args[4].toLowerCase().contains("batch")  || args[4].toLowerCase().contains("subset")
+				) ? EvlModuleDistributedFlinkSubset.class : EvlModuleDistributedFlinkSubset.class).getName().substring(20),
+			"int="+(args.length > 3 ? args[3] : "-1")
+		});
+	}
+	
+	public FlinkRunner(EvlRunConfiguration other) {
+		super(other);
+	}
+
+	@Override
+	protected EvlModuleDistributedMaster getDefaultModule() {
+		return new EvlModuleDistributedFlinkAtoms();
+	}
+}
--- plugins/org.eclipse.epsilon.evl.engine.distributed/src/org/eclipse/epsilon/evl/distributed/launch/DistributedRunner.java
+++ plugins/org.eclipse.epsilon.evl.engine.distributed/src/org/eclipse/epsilon/evl/distributed/launch/DistributedRunner.java
@@ -12,18 +12,14 @@ package org.eclipse.epsilon.evl.distributed.launch;
 import java.nio.file.Path;
 import java.util.Map;
 import org.eclipse.epsilon.common.util.StringProperties;
-import org.eclipse.epsilon.eol.cli.EolConfigParser;
+import org.eclipse.epsilon.eol.exceptions.EolRuntimeException;
 import org.eclipse.epsilon.eol.models.IModel;
 import org.eclipse.epsilon.evl.distributed.*;
 import org.eclipse.epsilon.evl.distributed.context.EvlContextDistributedMaster;
-import org.eclipse.epsilon.evl.distributed.flink.atomic.EvlModuleDistributedFlinkAtoms;
-import org.eclipse.epsilon.evl.distributed.flink.batch.EvlModuleDistributedFlinkSubset;
 import org.eclipse.epsilon.evl.launch.EvlRunConfiguration;
 
 /**
- * Run configuration container which serves two purposes: to
- * launch the master through command-line arguments by calling the
- * main method, and to hold the program arguments in the slave
+ * Run configuration container which holds the program arguments in the slave
  * nodes (i.e. the path to the script, the models, additional
  * parameters and arguments etc.).
  * 
@@ -31,27 +27,6 @@ import org.eclipse.epsilon.evl.launch.EvlRunConfiguration;
  * @since 1.6
  */
 public class DistributedRunner extends EvlRunConfiguration {
-	
-	public static void main(String[] args) throws ClassNotFoundException {
-		String modelPath = args[1].contains("://") ? args[1] : "file:///"+args[1];
-		String metamodelPath = args[2].contains("://") ? args[2] : "file:///"+args[2];
-		
-		EolConfigParser.main(new String[] {
-			"CONFIG:"+DistributedRunner.class.getName(),
-			args[0],
-			"-models",
-				"\"emf.DistributableEmfModel#"
-				+ "concurrent=true,cached=true,readOnLoad=true,storeOnDisposal=false,"
-				+ "modelUri="+modelPath+",fileBasedMetamodelUri="+metamodelPath+"\"",
-			args.length > 5 ? "-outfile" : "",
-			args.length > 5 ? args[5] : "",
-			"-module",
-				(args.length > 4 && (
-					args[4].toLowerCase().contains("batch")  || args[4].toLowerCase().contains("subset")
-				) ? EvlModuleDistributedFlinkSubset.class : EvlModuleDistributedFlinkSubset.class).getName().substring(20),
-			"int="+(args.length > 3 ? args[3] : "-1")
-		});
-	}
 	
 	public static Builder<DistributedRunner, ?> Builder() {
 		return Builder(DistributedRunner.class);
@@ -93,6 +68,11 @@ public class DistributedRunner extends EvlRunConfiguration {
 	
 	@Override
 	protected EvlModuleDistributedMaster getDefaultModule() {
-		return new EvlModuleDistributedFlinkAtoms();
+		return new EvlModuleDistributedMaster(1) {
+			@Override
+			protected void checkConstraints() throws EolRuntimeException {
+				throw new UnsupportedOperationException("This is a no-op module.");
+			}
+		};
 	}
 }
