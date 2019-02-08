486592f7dcf0533bb8b5afaefdd336d1216a2539
SMadani <sinadoom@googlemail.com>
2019-02-08 12:09:22 +0000

    Messaging-based prototype for distributed EVL.

---
+++ plugins/org.eclipse.epsilon.evl.engine.distributed.jms/src/org/eclipse/epsilon/evl/distributed/jms/EvlJMSWorker.java
@@ -0,0 +1,177 @@
+/*********************************************************************
+ * Copyright (c) 2019 The University of York.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+**********************************************************************/
+package org.eclipse.epsilon.evl.distributed.jms;
+
+import static org.eclipse.epsilon.evl.distributed.jms.EvlModuleDistributedMasterJMS.*;
+import java.io.Serializable;
+import java.net.UnknownHostException;
+import java.util.Map;
+import javax.jms.*;
+import org.apache.activemq.artemis.jms.client.ActiveMQJMSConnectionFactory;
+import org.eclipse.epsilon.common.function.CheckedConsumer;
+import org.eclipse.epsilon.eol.exceptions.EolRuntimeException;
+import org.eclipse.epsilon.evl.distributed.EvlModuleDistributedSlave;
+import org.eclipse.epsilon.evl.distributed.context.EvlContextDistributedSlave;
+import org.eclipse.epsilon.evl.distributed.data.DistributedEvlBatch;
+import org.eclipse.epsilon.evl.distributed.data.SerializableEvlInputAtom;
+import org.eclipse.epsilon.evl.distributed.launch.DistributedRunner;
+
+public class EvlJMSWorker implements Runnable {
+
+	public static void main(String[] args) throws UnknownHostException {
+		new EvlJMSWorker(args[0]).run();
+	}
+	
+	@Override
+	public void run() {
+		try {
+			setup();
+			processJobs();
+			//container.postExecute();
+			teardown();
+		}
+		catch (Exception ex) {
+			throw new RuntimeException(ex);
+		}
+	}
+	
+	final ConnectionFactory connectionFactory;
+	DistributedRunner configContainer;
+	String workerID;
+	volatile boolean finished;
+	final Object completionLock = new Object();
+
+	public EvlJMSWorker(String host) {
+		connectionFactory = new ActiveMQJMSConnectionFactory(host);
+	}
+	
+	void setup() throws Exception {
+		try (JMSContext regContext = connectionFactory.createContext()) {
+			// Announce our presence to the master
+			Destination regQueue = regContext.createQueue(REGISTRATION_QUEUE_NAME);
+			Destination tempQueue = regContext.createTemporaryQueue();
+			JMSProducer regProducer = regContext.createProducer();
+			Message initMsg = regContext.createMessage();
+			initMsg.setJMSReplyTo(tempQueue);
+			regProducer.send(regQueue, initMsg);
+			
+			// Get the configuration and our ID from the reply
+			Message configMsg = regContext.createConsumer(tempQueue).receive();
+			this.workerID = configMsg.getJMSCorrelationID();
+			System.out.println(workerID + " config and ID received at "+System.currentTimeMillis());
+			
+			configContainer = EvlContextDistributedSlave.parseJobParameters(configMsg.getBody(Map.class));
+			configContainer.preExecute();
+			((EvlModuleDistributedSlave)configContainer.getModule()).prepareExecution();
+			
+			// This is to acknowledge when we have completed loading the script(s) and model(s)
+			Message configuredAckMsg = regContext.createMessage();
+			configuredAckMsg.setJMSCorrelationID(workerID);
+			configuredAckMsg.setJMSReplyTo(tempQueue);
+			// Tell the master we're setup and ready to work
+			regProducer.send(configMsg.getJMSReplyTo(), configuredAckMsg);
+		}
+	}
+	
+	void processJobs() throws JMSException {
+		try (JMSContext jobContext = connectionFactory.createContext()) {
+			// Job processing, requires destinations for inputs (jobs) and outputs (results)
+			Destination jobDest = jobContext.createTopic(workerID + JOB_SUFFIX);
+			Queue resultsQueue = jobContext.createQueue(RESULTS_QUEUE_NAME);
+			JMSProducer resultsSender = jobContext.createProducer();
+			
+			CheckedConsumer<Serializable, JMSException> resultProcessor = obj -> {
+				ObjectMessage resultsMessage = jobContext.createObjectMessage(obj);
+				resultsMessage.setJMSCorrelationID(workerID);
+				resultsSender.send(resultsQueue, resultsMessage);
+			};
+			
+			jobContext.createConsumer(jobDest).setMessageListener(
+				getJobProcessor(resultProcessor, (EvlModuleDistributedSlave) configContainer.getModule())
+			);
+			
+			awaitCompletion();
+			
+			try (JMSContext finishedContext = jobContext.createContext(JMSContext.AUTO_ACKNOWLEDGE)) {
+				// Tell the master we've finished
+				Message finishedMsg = finishedContext.createMessage();
+				finishedMsg.setJMSCorrelationID(workerID);
+				Destination finishedDest = finishedContext.createQueue(RESULTS_QUEUE_NAME);
+				finishedContext.createProducer().send(finishedDest, finishedMsg);
+			}
+		}
+	}
+	
+	void awaitCompletion() {
+		System.out.println(workerID+" awaiting jobs since "+System.currentTimeMillis());
+		while (!finished) {
+			try {
+				synchronized (completionLock) {
+					completionLock.wait();
+				}
+			}
+			catch (InterruptedException ex) {
+				// TODO Auto-generated catch block
+				ex.printStackTrace();
+			}
+		}
+		System.out.println(workerID+" finished jobs at "+System.currentTimeMillis());
+	}
+	
+	protected MessageListener getJobProcessor(final CheckedConsumer<Serializable, ? extends JMSException> resultProcessor, final EvlModuleDistributedSlave module) {
+		return msg -> {
+			try {
+				if (!(msg instanceof ObjectMessage)) {
+					// End of jobs
+					finished = true;
+					System.out.println(workerID+" received all jobs by "+System.currentTimeMillis());
+					synchronized (completionLock) {
+						completionLock.notify();
+					}
+					return;
+				}
+				
+				final Serializable objMsg = ((ObjectMessage)msg).getObject();
+				final Object resultObj;
+				
+				if (objMsg instanceof SerializableEvlInputAtom) {
+					resultObj  = module.evaluateElement((SerializableEvlInputAtom) objMsg);
+				}
+				else if (objMsg instanceof DistributedEvlBatch) {
+					resultObj = module.evaluateBatch((DistributedEvlBatch) objMsg);
+				}
+				else {
+					resultObj = null;
+					System.err.println("["+workerID+"] Received unexpected object of type "+objMsg.getClass().getName());
+				}
+				
+				if (resultObj instanceof Serializable) {
+					resultProcessor.acceptThrows((Serializable) resultObj);
+					if (finished) {
+						// Wake up the main thread once the last job has been processed and sent
+						synchronized (completionLock) {
+							completionLock.notify();
+						}
+					}
+				}
+			}
+			catch (JMSException | EolRuntimeException ex) {
+				// TODO Auto-generated catch block
+				ex.printStackTrace();
+			}
+		};
+	}
+
+	void teardown() throws Exception {
+		if (connectionFactory instanceof AutoCloseable) {
+			((AutoCloseable) connectionFactory).close();
+		}
+	}
+}
---
+++ plugins/org.eclipse.epsilon.evl.engine.distributed.jms/src/org/eclipse/epsilon/evl/distributed/jms/EvlModuleDistributedMasterJMS.java
@@ -0,0 +1,287 @@
+/*********************************************************************
+ * Copyright (c) 2019 The University of York.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+**********************************************************************/
+package org.eclipse.epsilon.evl.distributed.jms;
+
+import java.io.Serializable;
+import java.net.InetAddress;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
+import javax.jms.*;
+import org.apache.activemq.artemis.jms.client.ActiveMQJMSConnectionFactory;
+import org.eclipse.epsilon.eol.cli.EolConfigParser;
+import org.eclipse.epsilon.eol.exceptions.EolRuntimeException;
+import org.eclipse.epsilon.evl.distributed.EvlModuleDistributedMaster;
+import org.eclipse.epsilon.evl.distributed.context.EvlContextDistributedMaster;
+import org.eclipse.epsilon.evl.distributed.data.DistributedEvlBatch;
+import org.eclipse.epsilon.evl.distributed.data.SerializableEvlResultAtom;
+import org.eclipse.epsilon.evl.distributed.launch.DistributedRunner;
+import org.eclipse.epsilon.evl.execute.UnsatisfiedConstraint;
+
+public class EvlModuleDistributedMasterJMS extends EvlModuleDistributedMaster {
+	
+	public static void main(String[] args) throws ClassNotFoundException, UnknownHostException, URISyntaxException {
+		String modelPath = args[1].contains("://") ? args[1] : "file:///"+args[1];
+		String metamodelPath = args[2].contains("://") ? args[2] : "file:///"+args[2];
+		String expectedWorkers = args[3];
+		String addr = args.length > 4 ? args[4] : null;
+		
+		if (addr == null || addr.length() < 5) {
+			addr = "tcp://"+InetAddress.getLocalHost().getHostAddress()+":61616";
+		}
+		
+		// For validation purposes
+		new URI(addr);
+		
+		EolConfigParser.main(new String[] {
+			"CONFIG:"+DistributedRunner.class.getName(),
+			args[0],
+			"-models",
+				"\"emf.DistributableEmfModel#"
+				+ "concurrent=true,cached=true,readOnLoad=true,storeOnDisposal=false,"
+				+ "modelUri="+modelPath+",fileBasedMetamodelUri="+metamodelPath+"\"",
+			"-module", EvlModuleDistributedMasterJMS.class.getName().substring(20),
+				"int="+expectedWorkers,
+				"String="+addr
+		});
+	}
+	
+	public static final String
+		REGISTRATION_QUEUE_NAME = "registration",
+		RESULTS_QUEUE_NAME = "results",
+		JOB_SUFFIX = "-jobs",
+		WORKER_ID_PREFIX = "EVL-jms-";
+	
+	private static final String LOG_PREFIX = "[MASTER] ";
+	
+	final String host;
+	final ConnectionFactory connectionFactory;
+	final List<Worker> workers;
+	final AtomicInteger workersFinished = new AtomicInteger();
+	JMSContext resultsContext;
+	Destination resultsDest;
+	
+	class Worker implements AutoCloseable {
+		public final String id;
+		public Destination localBox = null;
+		public boolean finished = false;
+		JMSContext session;
+		Topic jobsTopic;
+		JMSProducer jobSender;
+		
+		public Worker(String id) {
+			this.id = id;
+		}
+		
+		public void confirm(JMSContext session) {
+			jobsTopic = (this.session = session).createTopic(id + JOB_SUFFIX);
+			jobSender = session.createProducer();
+		}
+		
+		public void sendJob(Serializable input, JMSContext jobContext) throws JMSException {
+			ObjectMessage jobMsg = jobContext.createObjectMessage(input);
+			jobMsg.setJMSCorrelationID(id);
+			jobSender.send(jobsTopic, jobMsg);
+		}
+		
+		public void signalEnd(JMSContext jobContext) throws JMSException {
+			Message endMsg = jobContext.createMessage();
+			endMsg.setJMSCorrelationID(id);
+			jobSender.send(jobsTopic, endMsg);
+		}
+		
+		@Override
+		public void close() throws Exception {
+			session.close();
+			session = null;
+			jobsTopic = null;
+			jobSender = null;
+		}
+		
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj) return true;
+			if (!(obj instanceof Worker)) return false;
+			return Objects.equals(this.id, ((Worker)obj).id);
+		}
+		@Override
+		public int hashCode() {
+			return Objects.hashCode(id);
+		}
+		@Override
+		public String toString() {
+			return getClass().getName()+"-"+id;
+		}
+	}
+	
+	public EvlModuleDistributedMasterJMS(int expectedWorkers, String host) throws URISyntaxException {
+		super(expectedWorkers);
+		connectionFactory = new ActiveMQJMSConnectionFactory(this.host = host);
+		System.out.println(LOG_PREFIX+"connected to "+host+" at "+System.currentTimeMillis());
+		workers = new ArrayList<>(expectedWorkers);
+	}
+	
+	void awaitWorkers(final int expectedWorkers, final Serializable config) throws JMSException {
+		AtomicInteger connectedWorkers = new AtomicInteger();
+		
+		try (JMSContext regContext = connectionFactory.createContext()) {
+			// Initial registration of workers
+			Destination regQueue = regContext.createQueue(REGISTRATION_QUEUE_NAME);
+			JMSConsumer regConsumer = regContext.createConsumer(regQueue);
+			JMSProducer regProducer = regContext.createProducer();
+			
+			regConsumer.setMessageListener(msg -> {
+				try {
+					synchronized (connectedWorkers) {
+						// Assign worker ID
+						Destination tempQueue = regContext.createTemporaryQueue();
+						Worker worker = new Worker(WORKER_ID_PREFIX+connectedWorkers.get());
+						worker.localBox = msg.getJMSReplyTo();
+						workers.add(worker);
+						// Tell the worker what their ID is along with the configuration parameters
+						Message configMsg = regContext.createObjectMessage(config);
+						configMsg.setJMSReplyTo(tempQueue);
+						configMsg.setJMSCorrelationID(worker.id);
+						regProducer.send(worker.localBox, configMsg);
+						
+						// Wait for the worker to load the configuration and get back to us
+						Message ack = regContext.createConsumer(tempQueue).receive();
+						worker.confirm(regContext.createContext(JMSContext.AUTO_ACKNOWLEDGE));
+						
+						// Stop waiting once all workers are connected
+						if (connectedWorkers.incrementAndGet() == expectedWorkers) {
+							assert expectedWorkers == workers.size();
+							connectedWorkers.notify();
+						}
+					}
+				}
+				catch (JMSException jmx) {
+					throw new JMSRuntimeException(jmx.getMessage());
+				}
+			});
+			
+			// Wait for workers to confirm that they've completed configuration
+			while (connectedWorkers.get() < expectedWorkers) {
+				synchronized (connectedWorkers) {
+					try {
+						connectedWorkers.wait(30_000);
+					}
+					catch (InterruptedException ie) {}
+				}
+			}
+			
+			System.out.println(LOG_PREFIX+"Workers ready at "+System.currentTimeMillis());
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	protected MessageListener getResultsMessageListener() {
+		final Collection<UnsatisfiedConstraint> unsatisfiedConstraints = getContext().getUnsatisfiedConstraints();
+		return msg -> {
+			try {
+				if (msg instanceof ObjectMessage) {
+					Serializable contents = ((ObjectMessage)msg).getObject();
+					if (contents instanceof Iterable) {
+						for (SerializableEvlResultAtom sra : (Iterable<? extends SerializableEvlResultAtom>)contents) {
+							unsatisfiedConstraints.add(deserializeResult(sra));
+						}
+					}
+					else if (contents instanceof SerializableEvlResultAtom) {
+						unsatisfiedConstraints.add(deserializeResult((SerializableEvlResultAtom) contents));
+					}
+					else {
+						System.err.println(LOG_PREFIX+"Received unexpected object of type "+contents.getClass().getSimpleName());
+					}
+				}
+				else {
+					String workerID = msg.getJMSCorrelationID();
+					workers.stream().filter(w -> w.id.equals(workerID)).findAny().ifPresent(w -> {
+						w.finished = true;
+						if (workersFinished.incrementAndGet() >= workers.size()) {
+							synchronized (workersFinished) {
+								workersFinished.notify();
+							}
+						}
+					});
+				}
+			}
+			catch (JMSException jmx) {
+				throw new JMSRuntimeException(jmx.getMessage());
+			}
+		};
+	}
+	
+	void teardown() throws Exception {
+		resultsContext.close();
+		resultsContext = null;
+		if (connectionFactory instanceof AutoCloseable) {
+			((AutoCloseable) connectionFactory).close();
+		}
+	}
+	
+	@Override
+	protected void prepareExecution() throws EolRuntimeException {
+		super.prepareExecution();
+		try {
+			EvlContextDistributedMaster context = getContext();
+			awaitWorkers(context.getDistributedParallelism(), context.getJobParameters());
+			resultsContext = connectionFactory.createContext();
+			resultsDest = resultsContext.createQueue(RESULTS_QUEUE_NAME);
+			resultsContext.createConsumer(resultsDest).setMessageListener(getResultsMessageListener());
+		}
+		catch (Exception ex) {
+			throw ex instanceof EolRuntimeException ? (EolRuntimeException) ex : new EolRuntimeException(ex);
+		}
+	}
+	
+	@Override
+	protected void checkConstraints() throws EolRuntimeException {	
+		List<DistributedEvlBatch> batches = DistributedEvlBatch.getBatches(this);
+		Iterator<Worker> workersIter = workers.iterator();
+		Iterator<? extends Serializable> batchesIter = batches.iterator();
+		
+		try (JMSContext jobContext = connectionFactory.createContext()) {
+			while (batchesIter.hasNext() && workersIter.hasNext()) {
+				Worker worker = workersIter.next();
+				worker.sendJob(batchesIter.next(), jobContext);
+				worker.signalEnd(jobContext);
+				System.out.println(LOG_PREFIX+" finished submitting to "+worker);
+			}
+			
+			System.out.println(LOG_PREFIX+" awaiting completion...");
+			while (workersFinished.get() < workers.size()) {
+				synchronized (workersFinished) {
+					workersFinished.wait();
+				}
+			}
+			System.out.println(LOG_PREFIX+" completed.");
+		}
+		catch (JMSException | InterruptedException ex) {
+			throw new EolRuntimeException(ex);
+		}
+	}
+	
+	@Override
+	protected void postExecution() throws EolRuntimeException {
+		super.postExecution();
+		try {
+			teardown();
+		}
+		catch (Exception ex) {
+			throw ex instanceof EolRuntimeException ? (EolRuntimeException) ex : new EolRuntimeException(ex);
+		}
+	}
+}
