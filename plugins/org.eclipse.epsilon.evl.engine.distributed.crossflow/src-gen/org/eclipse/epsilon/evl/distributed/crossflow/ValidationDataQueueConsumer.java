0eecd03777353f241d1ebb002beb99d9fed0e903
SMadani <sinadoom@googlemail.com>
2019-06-03 17:14:35 +0100

    Removed distributed EVL

--- plugins/org.eclipse.epsilon.evl.engine.distributed.crossflow/src-gen/org/eclipse/epsilon/evl/distributed/crossflow/ValidationDataQueueConsumer.java
+++
@@ -1,9 +0,0 @@
-package org.eclipse.epsilon.evl.distributed.crossflow;
-
-import javax.jms.Message;
-
-public interface ValidationDataQueueConsumer {
-	
-	void consumeValidationDataQueueWithNotifications(ValidationData validationData) throws Exception;
-	
-}
